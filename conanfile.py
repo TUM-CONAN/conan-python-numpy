4dc0125192d12ffaa69cfd40b69a40b4122e4af9
Ulrich Eck <ulrich.eck@tum.de>
2020-05-07 10:39:38 +0200

    python numpy now installs its artifacts correctly

---
+++ conanfile.py
@@ -0,0 +1,30 @@
+import os
+
+from conans import ConanFile, tools
+
+
+class PythonNumpyConan(ConanFile):
+    name = "python-numpy"
+    version = tools.get_env("GIT_TAG", "1.18.4")
+    license = "MIT"
+    description = "Numpy extension for Python Interpreter"
+    settings = "os", "compiler", "build_type", "arch"
+
+    def source(self):
+        tools.get("https://github.com/numpy/numpy/releases/download/v%s/numpy-%s.tar.gz" % (self.version, self.version))
+
+    def build_requirements(self):
+        self.build_requires("generators/1.0.0@camposs/stable")
+        self.build_requires("python-setuptools/[>=41.2.0]@camposs/stable")
+        self.build_requires("python-pip/[>=19.2.3]@camposs/stable")
+
+    def requirements(self):
+        self.requires("python/[>=3.8.2]@camposs/stable")
+        self.requires("clapack/3.2.1@camposs/stable")
+
+    def build(self):
+        with tools.chdir("numpy-" + self.version):
+            self.run('pip install --install-option="--prefix=\"%s\"" .' % self.package_folder)
+
+    def package_info(self):
+        self.env_info.PYTHONPATH.append(os.path.join(self.package_folder, "lib", "python3.8", "site-packages"))
